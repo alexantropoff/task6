9149c482b03b14d139956dca927e59097f4b20a2
Alex Antropoff <alant@idc.md>
2023-03-17 09:30:27 +0200

    1st

--- task6/ViewController.swift
+++ task6/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let squareView = UIView()
+    var animator: UIDynamicAnimator!
+    var snapping: UISnapBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        squareView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        squareView.center = view.center
+        squareView.backgroundColor = .blue
+        squareView.layer.cornerRadius=10
+        view.addSubview(squareView)
+        animator=UIDynamicAnimator(referenceView: view)
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        
+        let touchPoint = touch.location(in: view)
+        animator.removeAllBehaviors()
+        snapping = UISnapBehavior(item: squareView, snapTo: touchPoint)
+        animator.addBehavior(snapping)
+    }
+    
+    
 }
-
